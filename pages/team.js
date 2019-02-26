fcb23c6f312f0191b9f0f16277b3f6f7f19f9e80
josearmando1470 <josearmando1470@gmail.com>
2019-02-26 01:10:58 -0600

    optimization of images and finalized page

--- pages/team.js
+++ pages/team.js
@@ -27,6 +27,23 @@ class Team extends React.Component {
             </div>
           </div>
           <div className="row ">
+
+          <div className="col s12 m3">
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/fawkes.png"></img>
+                <span className="card-title">Jon Fawkes Greger</span>
+              </div>
+              <div className=" center">
+                <p><a href="#">{this.props.t('fawkes-employee')}</a></p>
+                <a href="mailto:fawkes@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
+              </div>
+              <div className="card-content">
+                <p>{this.props.t('fawkes-description')}</p>
+              </div>
+            </div>
+          </div>
+
           <div className="col s12 m3">
             <div className="card">
               <div className="card-medium center">
@@ -46,55 +63,41 @@ class Team extends React.Component {
             </div>
           </div>
 
+        
           <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
-                <img src="/static/images/ahgala.png"></img>
-                <span class="card-title">Alvaro Hernández</span>
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/alex.png"></img>
+                <span className="card-title">Alexandre Ederer</span>
               </div>
-              <div class=" center">
+              <div className=" center">
+                <p><a href="#">{this.props.t('alex-employee')}</a></p>
+                <a className="linkedin" href="https://www.linkedin.com/in/alexandre-ederer/" target="_blank"><img src="/static/images/linkedin.png" alt="LinkedIn"></img></a>
+                <a href="mailto:alex@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
+              </div>
+              <div className="card-content">
+                <p>{this.props.t('alex-description')}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col s12 m3">
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/ahgala.png"></img>
+                <span className="card-title">Alvaro Hernández</span>
+              </div>
+              <div className=" center">
                 <p><a href="#">{this.props.t('alvaro-employee')}</a></p>
                 <a className="linkedin" href="https://www.linkedin.com/in/alhega" target="_blank"><img src="/static/images/linkedin.png" alt="LinkedIn"></img></a>
                 <a href="https://gitlab.com/ahgala" target="_blank"><img src="/static/images/gitlab.png" alt="GitLab"></img></a>
                 <a href="mailto:alvaro@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
               </div>
-              <div class="card-content">
+              <div className="card-content">
                 <p>{this.props.t('alvaro-description')}</p>
               </div>
             </div>
           </div>
-
-          <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
-                <img src="/static/images/alex.png"></img>
-                <span class="card-title">Alexandre Ederer</span>
-              </div>
-              <div class=" center">
-                <p><a href="#">{this.props.t('alex-employee')}</a></p>
-                <a className="linkedin" href="https://www.linkedin.com/in/alexandre-ederer/" target="_blank"><img src="/static/images/linkedin.png" alt="LinkedIn"></img></a>
-                <a href="mailto:alex@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
-              </div>
-              <div class="card-content">
-                <p>{this.props.t('alex-description')}</p>
-              </div>
-            </div>
-          </div>
-          
-          <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
-                <img src="/static/images/fawkes.png"></img>
-                <span class="card-title">Fawkes</span>
-              </div>
-              <div class=" center">
-                <p><a href="#">{this.props.t('fawkes-employee')}</a></p>
-              </div>
-              <div class="card-content">
-              </div>
-            </div>
-          </div>
-
 
           
         </div>
@@ -103,56 +106,91 @@ class Team extends React.Component {
 
 
           <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
+            <div className="card">
+              <div className="card-medium center">
                 <img src="/static/images/let.png"></img>
-                <span class="card-title">Osmar Pérez Bautista</span>
+                <span className="card-title">Osmar Pérez Bautista</span>
               </div>
-              <div class=" center">
+              <div className=" center">
                 <p><a href="#">{this.props.t('let-employee')}</a></p>
                 <a href="https://gitlab.com/osmarpb.97" target="_blank"><img src="/static/images/gitlab.png" alt="GitLab"></img></a>
                 <a href="mailto:osmar@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
               </div>
-              <div class="card-content">
+              <div className="card-content">
                 <p>{this.props.t('let-description')}</p>
               </div>
             </div>
           </div>
 
           <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
-                <img src="/static/images/dx.jpeg"></img>
-                <span class="card-title">Luis Alberto Saavedra Nieto</span>
-              </div>
-              <div class=" center">
-                <p><a href="#">{this.props.t('jose-employee')}</a></p>
-                <a href="mailto:alberto@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
-              </div>
-              <div class="card-content">
-                <p>Redactor with social media habilities and advertising campaigns. Focused on enterprise writing, documents redaction and edition</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="col s12 m3">
-            <div class="card">
-              <div class="card-medium center">
+            <div className="card">
+              <div className="card-medium center">
                 <img src="/static/images/jose.jpg"></img>
-                <span class="card-title">José Armando Rivera Ramírez</span>
+                <span className="card-title">José Armando Rivera Ramírez</span>
               </div>
-              <div class=" center">
+              <div className=" center">
                 <p><a href="#">{this.props.t('jose-employee')}</a></p>
                 <a href="https://gitlab.com/josearmando1470" target="_blank"><img src="/static/images/gitlab.png" alt="GitLab"></img></a>
                 <a href="mailto:jose@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
               </div>
-              <div class="card-content">
+              <div className="card-content">
                 <p>{this.props.t('jose-description')}</p>
               </div>
             </div>
           </div>
 
+          <div className="col s12 m3">
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/dx.jpeg"></img>
+                <span className="card-title">Luis Alberto Saavedra Nieto</span>
+              </div>
+              <div className=" center">
+                <p><a href="#">{this.props.t('dx-employee')}</a></p>
+                <a href="mailto:alberto@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
+              </div>
+              <div className="card-content">
+                <p>{this.props.t('dx-description')}</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="col s12 m3">
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/jesi.jpg"></img>
+                <span className="card-title">Jessica Reyes Espinosa</span>
+              </div>
+              <div className=" center">
+                <p><a href="#">{this.props.t('jesi-employee')}</a></p>
+                <a className="linkedin" href="https://www.linkedin.com/in/jessica-reyes-espinosa-345150115/" target="_blank"><img src="/static/images/linkedin.png" alt="LinkedIn"></img></a>
+                <a href="mailto:jessica@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
+              </div>
+              <div className="card-content">
+                <p>{this.props.t('jesi-description')}</p>
+              </div>
+            </div>
+          </div>
+
+
         </div>
+          <div className="row">
+          <div className="col s12 m3">
+            <div className="card">
+              <div className="card-medium center">
+                <img src="/static/images/Guty.jpg"></img>
+                <span className="card-title">Jesus Alberto Gutierrez Castelan</span>
+              </div>
+              <div className=" center">
+                <p><a href="#">{this.props.t('guty-employee')}</a></p>
+                <a href="mailto:guty@neetsec.com" target="_blank"><img className="mail" src="/static/images/mail.png" alt="GitLab"></img></a>
+              </div>
+              <div className="card-content">
+                <p>{this.props.t('guty-description')}</p>
+              </div>
+            </div>
+          </div>
+          </div>
           
         </div>
         <Footer />
